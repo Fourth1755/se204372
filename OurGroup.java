4a3e138530859abc055a31aa67cc90796156e525
NuttawatJuntawong <91516482+NuttawatJuntawong@users.noreply.github.com>
2022-01-25 21:19:47 +0700

    My first group changes to OurGroup.java

--- OurGroup.java
+++ OurGroup.java
@@ -7,17 +7,11 @@ public class OurGroup {
     {
         groupMembers = new Vector<String>();
         groupMembers.add("Thanapat Wongtungjaroensuk");
-<<<<<<< HEAD
-<<<<<<< HEAD
 	groupMembers.add("Nuttawat Juntawong");
-=======
-	    groupMembers.add("Nuttawat Juntawong"); 
->>>>>>> 76d989c772318f864215a6c7d2b48d2599a43a1e
-=======
-	groupMembers.add("Nuttawat Juntawong"); 
->>>>>>> d88920331e7607a0e2a9d563d05da271a2bbf8a1
+	groupMembers.add("Nuttawat Juntawong");
         groupMembers.add("Pooriwat Poolab");//PUT YOUR NAME HERE
-        groupMembers.add("Lalita Apirakausanee");//PUT YOUR NAME HERE
+        groupMembers.add("Lalita Apirakausanee");
+	groupMembers.add("Chatchaya Promrin");//PUT YOUR NAME HERE
 
     }
     public Vector<String> getGroupMembers () {
